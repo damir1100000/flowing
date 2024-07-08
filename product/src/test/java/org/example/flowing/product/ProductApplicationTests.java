b5d227604b2847e84165528261a7cc077840ed2e
Damir Achimovici <113127218+damir1100000@users.noreply.github.com>
2024-07-08 13:36:53 +0300

    JAR files for deployment

--- product/src/test/java/org/example/flowing/product/ProductApplicationTests.java
+++ product/src/test/java/org/example/flowing/product/ProductApplicationTests.java
@@ -2,12 +2,14 @@ package org.example.flowing.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-class ProductApplicationTests {
+@ActiveProfiles("test")
+public class ProductApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
-
 }
